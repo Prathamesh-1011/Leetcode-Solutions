6f2cabd9c8e8a529ab22872a3a05fc1815343de1
Prathamesh Rokade <144042848+Prathamesh-1011@users.noreply.github.com>
2025-03-11 20:07:59 +0530

    Time: 1 ms (95.93%), Space: 40.8 MB (20.68%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num <= 1) return false; 
+
+        int sum = 1;
+        int sqrt = (int) Math.sqrt(num);
+
+        for (int i = 2; i <= sqrt; i++) {
+            if (num % i == 0) {
+                sum += i;
+                int correspondingDivisor = num / i;
+                if (i != correspondingDivisor) {
+                    sum += correspondingDivisor;
+                }
+            }
+        }
+
+        return sum == num;
+    }
+}
