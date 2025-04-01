794106d85dfc98ed60521f615816ae9b65f428cc
Prathamesh Rokade <144042848+Prathamesh-1011@users.noreply.github.com>
2025-04-01 20:01:35 +0530

    Time: 22 ms (99.51%), Space: 47.4 MB (99.91%) - LeetHub

---
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.java
@@ -0,0 +1,20 @@
+class Solution {
+        Queue<Integer> queue = new LinkedList<>();
+
+        public Solution(int[] w) {
+            int sum = 0;
+            
+            for (int num : w) sum += num;
+
+            for (int i = 0; i < w.length; i++) {
+                for (int j = 0; j < ((w[i]*100)/sum); j++) queue.add(i);
+            }
+        }
+
+        public int pickIndex() {
+            if(queue.isEmpty()) return 0;
+            int num = queue.poll();
+            queue.add(num);
+            return num;
+        }
+    }
