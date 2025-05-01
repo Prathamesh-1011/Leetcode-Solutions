f9a5e6c11bef6321c9b6bf39ce3f566a3edecd3a
Prathamesh Rokade <144042848+Prathamesh-1011@users.noreply.github.com>
2025-05-01 15:41:15 +0530

    Time: 46 ms (90.2%), Space: 55.4 MB (71.02%) - LeetHub

--- 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.java
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.java
@@ -1,39 +1,47 @@
+import java.util.*;
+
 class Solution {
     public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
-        int left = 0, right = Math.min(tasks.length, workers.length);
         Arrays.sort(tasks);
         Arrays.sort(workers);
-        while (left < right) {
-            int mid = (left + right + 1) / 2;
-            int usedPills = 0;
-            TreeMap<Integer, Integer> avail = new TreeMap<>();
-            for (int i = workers.length - mid; i < workers.length; ++i)
-                avail.put(workers[i], avail.getOrDefault(workers[i], 0) + 1);
-            boolean canAssign = true;
-            for (int i = mid - 1; i >= 0; --i) {
-                int t = tasks[i];
-                int w = avail.lastKey();
-                if (w >= t) {
-                    decrement(avail, w);
-                } else {
-                    Integer key = avail.ceilingKey(t - strength);
-                    if (key == null || ++usedPills > pills) {
-                        canAssign = false;
-                        break;
-                    }
-                    decrement(avail, key);
-                }
+        int low = 0, high = Math.min(tasks.length, workers.length);
+
+        while (low < high) {
+            int mid = (low + high + 1) / 2;
+            if (canAssign(tasks, workers, pills, strength, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
             }
-            if (canAssign)
-                left = mid;
-            else
-                right = mid - 1;
         }
-        return left;
+
+        return low;
     }
-    private void decrement(TreeMap<Integer, Integer> m, int k) {
-        int c = m.get(k);
-        if (c == 1) m.remove(k);
-        else m.put(k, c - 1);
+
+    private boolean canAssign(int[] tasks, int[] workers, int pills, int strength, int taskCount) {
+        Deque<Integer> boosted = new ArrayDeque<>();
+        int w = workers.length - 1;
+        int freePills = pills;
+
+        for (int t = taskCount - 1; t >= 0; t--) {
+            int task = tasks[t];
+
+            if (!boosted.isEmpty() && boosted.peekFirst() >= task) {
+                boosted.pollFirst();
+            } else if (w >= 0 && workers[w] >= task) {
+                w--;
+            } else {
+                while (w >= 0 && workers[w] + strength >= task) {
+                    boosted.addLast(workers[w--]);
+                }
+                if (boosted.isEmpty() || freePills == 0) {
+                    return false;
+                }
+                boosted.pollLast();
+                freePills--;
+            }
+        }
+
+        return true;
     }
 }
