6f2c680b5f2ee7e49b1bea1fe48799bd20c71148
Prathamesh Rokade <144042848+Prathamesh-1011@users.noreply.github.com>
2025-06-03 19:53:50 +0530

    Time: 0 ms (100%), Space: 45.8 MB (57.87%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,36 @@
+class Solution {
+    static {
+        for(int i = 0; i < 500; ++i)
+            candy(new int[]{1,3,2});
+    }
+    public static int candy(int[] ratings) {
+        int n=ratings.length;
+        int i=1,sum=1;
+
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum=sum+1;
+                i++;
+                continue;
+            }
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak+=1;
+                sum+=peak;
+                i++;
+            }
+
+            int down =1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                down++;
+                i++;
+            }
+
+            if(down>peak){
+                sum=sum+(down-peak);
+            }
+        }
+        return sum;
+    }
+}
