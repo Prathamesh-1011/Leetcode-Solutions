15a50a8ca548e73ba509d5313e3b6ebd0943fcf1
Prathamesh Rokade <144042848+Prathamesh-1011@users.noreply.github.com>
2025-07-18 22:04:35 +0530

    Time: 163 ms (68.12%), Space: 79.8 MB (73.19%) - LeetHub

---
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.java
@@ -0,0 +1,36 @@
+class Solution {
+    public long minimumDifference(int[] nums) {
+        long ans = Long.MAX_VALUE;
+        int n = nums.length/3;
+        long minSum = 0, maxSum = 0;
+        
+        PriorityQueue<Integer> maxq = new PriorityQueue<>((a, b) -> nums[b] - nums[a]);
+        PriorityQueue<Integer> minq = new PriorityQueue<>((a, b) -> nums[a] - nums[b]);
+        
+        for (int i = 0; i < n; i++) { maxq.add(i); minSum += nums[i]; }
+        for (int i = 3*n - 1; i >= 2 * n; i--) { minq.add(i); maxSum += nums[i]; }
+        
+        long[] left = new long[n + 1]; left[0] = minSum;
+        long[] right = new long[n + 1]; right[n] = maxSum;
+        
+        for (int i = n; i < 2 * n; i++) {
+            if (nums[i] < nums[maxq.peek()]) {
+                minSum = minSum - nums[maxq.poll()] + nums[i];
+                maxq.add(i);
+            }
+            left[i - n + 1] = minSum;
+        }
+        
+        for (int i = 2*n - 1; i >= n; i--) {
+            if (nums[i] > nums[minq.peek()]) {
+                maxSum = maxSum - nums[minq.poll()] + nums[i];
+                minq.add(i);
+            }
+            right[i - n] = maxSum;
+        }
+        
+        for (int i = 0; i <= n; i++) ans = Math.min(ans, left[i] - right[i]);
+        
+        return ans;
+    }
+}
