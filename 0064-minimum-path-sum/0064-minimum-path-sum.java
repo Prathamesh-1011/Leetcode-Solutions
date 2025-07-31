24e63b7f3cb06bbe4dcf680136eeda92cd789485
Prathamesh Rokade <144042848+Prathamesh-1011@users.noreply.github.com>
2025-07-31 22:18:46 +0530

    Time: 1 ms (99.66%), Space: 48.1 MB (14.14%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,18 +1,32 @@
 class Solution {
-    private int rec(int r,int c,int m,int n,int[][] grid,int dp[][])
-    {
-        if(r==m && c==n) return grid[r][c];
-        if(dp[r][c]!=-1) return dp[r][c];
-        int right = Integer.MAX_VALUE, down = Integer.MAX_VALUE;
-        if(c+1<=n) right = rec(r,c+1,m,n,grid,dp);
-        if(r+1<=m) down = rec(r+1,c,m,n,grid,dp);
-        return dp[r][c] = grid[r][c] + Math.min(right,down);
-    }
     public int minPathSum(int[][] grid) {
-        int m= grid.length,n=grid[0].length;
-        int[][] dp = new int[m][n];
-        for(int[] row: dp) Arrays.fill(row,-1);
+        int n = grid.length;
+        int m = grid[0].length;
 
-        return rec(0,0,m-1,n-1,grid,dp);
+        int dp[][] = new int[n][m];
+        
+        // Initialize the DP matrix with -1
+        for (int row[] : dp)
+            Arrays.fill(row, -1);
+        
+        // Call the helper function starting from the bottom-right cell
+        return minSumPathUtil(n - 1, m - 1, grid, dp);
+    }
+
+    public int minSumPathUtil(int i, int j, int[][] matrix, int[][] dp) {
+        // Base cases
+        if (i == 0 && j == 0)
+            return matrix[0][0]; // If we're at the top-left cell, return its value
+        if (i < 0 || j < 0)
+            return (int) Math.pow(10, 9); // If we're out of bounds, return a large value
+        if (dp[i][j] != -1)
+            return dp[i][j]; // If we've already calculated this cell, return the stored result
+
+        // Calculate the sum of the current cell plus the minimum sum path from above and from the left
+        int up = matrix[i][j] + minSumPathUtil(i - 1, j, matrix, dp);
+        int left = matrix[i][j] + minSumPathUtil(i, j - 1, matrix, dp);
+
+        // Store the minimum of the two possible paths
+        return dp[i][j] = Math.min(up, left);
     }
 }
