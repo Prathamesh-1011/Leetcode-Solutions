10e741a72bf419482ce0755c59867cfe24ecc34a
Prathamesh Rokade <144042848+Prathamesh-1011@users.noreply.github.com>
2025-08-08 21:49:28 +0530

    Time: 87 ms (26.47%), Space: 57.1 MB (36.88%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,22 +1,40 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = 0, n = nums.length;
-        
-        for (int i = 0; i < n; i++)
+        int n = nums.length;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
             sum += nums[i];
-        
-        if (sum % 2 != 0) 
-            return false;
-
-        boolean[] dp = new boolean[10001];
-        dp[0] = true;
-        for (int num : nums) {
-            for (int j = 10000; j >= num; j--) {
-                dp[j] = dp[j] || dp[j - num];
-            }
-            if (dp[sum / 2]) 
-                return true;
         }
-        return dp[sum / 2];
+        if(sum % 2 == 1){
+            return false;
+        } else {
+            int k = sum / 2;
+            int dp[][] = new int[n][k+1];
+            for(int row[] : dp){
+                Arrays.fill(row,-1);
+            }
+            return sub(n-1,k,nums,dp);
+        }
+    }
+    public boolean sub(int ind, int k, int[] nums, int[][] dp){
+        if(k == 0){
+            return true;
+        }
+
+        if(ind == 0){
+            return nums[0] == k;
+        }
+
+        if (dp[ind][k] != -1)
+            return dp[ind][k] == 0 ? false : true;
+
+        boolean nottake = sub(ind-1,k,nums,dp);
+        boolean take = false;
+        if(nums[ind] <= k){
+            take = sub(ind-1,k-nums[ind],nums,dp);
+        }
+
+        dp[ind][k] = nottake || take ? 1 : 0;
+        return nottake || take;
     }
 }
