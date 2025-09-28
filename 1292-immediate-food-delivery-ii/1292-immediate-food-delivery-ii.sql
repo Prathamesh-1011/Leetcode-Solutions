6b91d05016f6edecb7a9883ed868fa15bc1d3a0d
Prathamesh Rokade <144042848+Prathamesh-1011@users.noreply.github.com>
2025-09-28 16:26:18 +0530

    Time: 651 ms (42.93%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,15 +1,12 @@
-# Write your MySQL query statement below
-WITH delivery_base AS
-(SELECT delivery_id
-,customer_id
-,order_date
-,customer_pref_delivery_date
-,RANK() OVER (PARTITION BY customer_id ORDER BY order_date ASC) AS rnk
-FROM delivery)
-
-,delivery_cnt AS
-(SELECT COUNT(CASE WHEN rnk = 1 AND order_date = customer_pref_delivery_date THEN customer_id END) AS immediate_cnt 
-,COUNT(CASE WHEN rnk = 1 THEN customer_id END) AS scheduled_cnt
-FROM delivery_base)
-
-SELECT ROUND((immediate_cnt/scheduled_cnt)*100,2) AS immediate_percentage FROM delivery_cnt
+SELECT 
+    ROUND(
+        SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100.0 
+        / COUNT(DISTINCT customer_id),
+        2
+    ) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
